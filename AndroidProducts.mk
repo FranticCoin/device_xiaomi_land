52387705c8dd3017692135a7dbd8b3909df1b136
FranticCoin <nikhilbaddi98@gmail.com>
2020-06-15 17:56:53 +0000

    land: Omni bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_land.mk
+    $(LOCAL_DIR)/omni_land.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_land-user \
-    aosp_land-userdebug \
-    aosp_land-eng
+    omni_land-user \
+    omni_land-userdebug \
+    omni_land-eng
