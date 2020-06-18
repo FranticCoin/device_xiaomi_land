be8c95a5581a645bc5c6a6b58c775d9ca6acb1af
FranticCoin <nikhilbaddi98@gmail.com>
2020-06-18 12:23:45 +0000

    land: Point to the right telephony makefiles

--- omni_land.mk
+++ omni_land.mk
@@ -17,8 +17,11 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common OmniROM stuff
 $(call inherit-product, vendor/omni/config/common.mk)
