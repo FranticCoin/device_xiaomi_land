019782c556738618c3e2e5fd13e7120126842e45
FranticCoin <nikhilbaddi98@gmail.com>
2020-12-18 09:16:04 +0000

    land: Remove prebuilt GCamGo Camera app

--- device.mk
+++ device.mk
@@ -128,8 +128,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    camera.msm8937 \
-    GCamGo
+    camera.msm8937
 
 # Configstore
 PRODUCT_PACKAGES += \
