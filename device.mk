24fc329e3fd67c4e6fe56238da94cef5ad3d040f
FranticCoin <nikhilbaddi98@gmail.com>
2020-12-27 17:36:38 +0000

    land: Build libavservices_minijail For, F/linker  (3736): CANNOT LINK EXECUTABLE "/vendor/bin/dspservice": library "libavservices_minijail.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -292,7 +292,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libavservices_minijail.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
