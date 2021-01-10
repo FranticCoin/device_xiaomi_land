d6484c2e9a53152a80b6f4b5cfdab7535549ab69
FranticCoin <nikhilbaddi98@gmail.com>
2021-01-10 11:17:10 +0000

    land: DotOS bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_land.mk
+    $(LOCAL_DIR)/dot_land.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_land-user \
-    aosp_land-userdebug \
-    aosp_land-eng
+    dot_land-user \
+    dot_land-userdebug \
+    dot_land-eng
