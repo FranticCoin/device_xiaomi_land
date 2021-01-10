0e503bfb27a7ba7547fc31ff31ef7e07b4c0cd1d
FranticCoin <nikhilbaddi98@gmail.com>
2021-01-10 11:42:40 +0000

    land: Add HALs path to Soong namespaces

--- device.mk
+++ device.mk
@@ -373,7 +373,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/msm8996
 
 # Telephony
 PRODUCT_PACKAGES += \
